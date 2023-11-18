02cfc4c0d990d7d3c7f8db133a3f95d9d6a923e3
FxA <furiousxangel@gmail.com>
2023-11-18 18:58:32 -0500

    added edit to frontend

--- webui/Public/index.js
+++ webui/Public/index.js
@@ -27,8 +27,6 @@ async function submitBookmark(e) {
     data[key] = value
   })
 
-  console.log("data = ", data)
-
   const fOptions = {
     method: "POST",
     cache: "no-cache",
@@ -61,39 +59,96 @@ function showBookmarks(bookmarks) {
 
 const deleteBookmark=(loopId, dbId)=> {
   
-  console.log("clickon: ", loopId)
-
   const ele = document.getElementById(loopId)
-
-  while (ele.firstChild) {
-    ele.removeChild(ele.firstChild)
-  }
 
   fetch(`http://localhost:8081/bookmarks/${dbId}`, {method:"delete"})
     .then((res)=>{
       if (!res.ok) {
         console.log("expected ok, received: ", res.status)
       }
+      while (ele.firstChild) {
+        ele.removeChild(ele.firstChild)
+      }
+      bookmarkList.splice(loopId, 1)
     })
     .catch((err)=>{
       console.log("error deleting bookmark; ", err)
     })
-
 }
 
 const addBookmark=(addedBookmark)=>{
   bookmarkList.push(addedBookmark)
   showBookmarks(bookmarkList)
+  document.getElementById("form-bookmark").reset()
+}
+
+const editBookmark=(loopId, dbId)=>{
+
+  const bmEl = document.getElementById(loopId)
+
+  bmEl.outerHTML = htmlEditBookmark(bookmarkList[loopId], loopId)
+}
+
+const saveBookmark=(bookmark)=>{
+
+  console.log("save bookmark: ", bookmarkList[bookmark])
+  const formData = new FormData(document.getElementById("editedBM"))
+  let data = {}
+
+  formData.forEach((value, key)=>{
+    data[key] = value
+  })
+
+  const fOptions = {
+    method: "PUT",
+    cache: "no-cache",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }
+
+  fetch(`http://localhost:8081/bookmarks/${bookmarkList[bookmark].rowid}`, fOptions)
+    .then((res)=>{
+      if (res.ok) {
+        return res.json()
+      }
+    })
+  .then((data)=>{
+     document.getElementById(bookmark).outerHTML = htmlBookmark(data, bookmark)
+  })
 }
 
 const htmlBookmark=(bookmark, i=1)=>{
   
   const html=
    `<div id=${i} class="bookmark">
-    <button type="button" class="button delete" onclick="deleteBookmark(${i},${bookmark.rowid})" >X</button>
-    <h3><a href=${bookmark.url} target="_blank">${bookmark.url}</a></h3>
-    -- ${bookmark.description}
+      <div class="button-group">
+        <button type="button" class="button delete" onclick="deleteBookmark(${i},${bookmark.rowid})" >X</button>
+        <button type="button" class="button edit" onclick="editBookmark(${i},${bookmark.rowid})" >E</button>
+      </div>
+      <div id="bmURL">
+        <h3><a href=${bookmark.url} target="_blank">${bookmark.url}</a></h3>
+      </div>
+      <p id="bmDesc">${bookmark.description}</p>
     </div>`
 
   return html
 }
+
+const htmlEditBookmark=(bookmark, i)=>{
+
+  const html=
+   `<div id=${i} class="bookmark">
+      <div class="button-group">
+      <button type="button" class="button save" onclick="saveBookmark(${i},${bookmark.rowid})">Save</button>
+      </div>
+      <form id="editedBM">
+        <input type="text" name="URL" value="${bookmarkList[i].url}">
+        <input type="text" name="Description" value="${bookmarkList[i].description}">
+      </form>
+    </div>`
+
+  return html
+}
+
